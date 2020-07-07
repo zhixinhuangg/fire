b860d0cbecfa2893c26095348389687b33fe3889
zhixin.huang <941006066@qq.com>
2020-07-07 10:40:08 +0800

    refactor

---
+++ src/main/java/com/dmsoft/fire/mapper/CommodityMapper.java
@@ -0,0 +1,13 @@
+package com.dmsoft.fire.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.dmsoft.fire.entity.Commodity;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author zhixin.huang
+ * @date 2020/07/07
+ */
+@Mapper
+public interface CommodityMapper extends BaseMapper<Commodity> {
+}
---
+++ src/main/java/com/dmsoft/fire/mapper/SysLogMapper.java
@@ -0,0 +1,13 @@
+package com.dmsoft.fire.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.dmsoft.fire.entity.SysLog;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @author zhixin.huang
+ * @date 2020/07/07
+ */
+@Mapper
+public interface SysLogMapper extends BaseMapper<SysLog> {
+}
